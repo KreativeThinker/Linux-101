243907f5ceaafaac625644189b14d18d6839a785
KreativeThinker <sagely2005eagle@gmail.com>
2024-07-06 15:41:08 +0530

    Updated files

--- Examples/execute.sh
+++ Examples/execute.sh
@@ -5,18 +5,18 @@
 while getopts ":f:" opt; do
   case $opt in
     f) FILE=$OPTARG ;;
-    \?) echo "Invalid option: -$OPTARG" >&2 ;;
+    \?) echo "Invalid option: -$OPTARG" >&2 ;; # >&2 redirected output as console error
   esac
 done
 
 if [ -z "$FILE" ]; then
-  echo "No file passed" >&2
+  echo "No file passed" >&2 # >&2 redirected output as console error
   exit 1
 fi
 
 if [ -f "$FILE" ]; then
   chmod +x "$FILE" && ./"$FILE"
 else
-  echo "File not found: $FILE" >&2
+  echo "File not found: $FILE" >&2 # >&2 redirected output as console error
   exit 1
 fi
--- Examples/modified.sh
+++ Examples/modified.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 # List modified files script
-find /path/to/search -type f -mtime -7
+find /home/ghost/tmp -type f -mtime -7
 
 # **Explanation**
 # Uses `find` to locate files modified in the past week.
